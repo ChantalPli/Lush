fcda1d45f7e3b00b7bc6a198ed30c88ba41941e8
Sergio Robledo <94855927+seergiorob@users.noreply.github.com>
2022-04-25 15:56:52 -0300

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const Strategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const passport = require('passport');
